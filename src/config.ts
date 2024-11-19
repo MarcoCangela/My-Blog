ec2a3da2fb396e3af528131d0ea09192c66dc473
Marco Cangela Garujo <Mcangelaoo@gmail.com>
2024-11-19 23:33:07 +0200

    Refining Header

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Marco Garujo's Blog";
+export const SITE_TITLE = "Marco's Blog";
 export const SITE_DESCRIPTION =
   "Welcome to my blog! I write abput Software Quality Assurance and Technology in general.";
 export const TWITTER_HANDLE = "@MarcoCangela";
